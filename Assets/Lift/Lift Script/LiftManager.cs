33d6dfadc12f6038ea0d7781b3fe46a584793a77
Yukinori-Kuwahara <chara.flarty999@gmail.com>
2025-05-22 12:33:33 +0900

    [add] リフトの追加

---
+++ Assets/Lift/Lift Script/LiftManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftManager : MonoBehaviour
+{
+    /// <summary>初期動作を設定できる。</summary>
+    [SerializeField, Tooltip("リフトの移動方向(斜め移動リフトの時は右のほうを参照)")] Liftmode liftmoving = default;
+    int _liftmode;
+    [SerializeField, Tooltip("プレイヤーが乗った時に作動するか")] bool playOnCollision = default;
+    [SerializeField, Tooltip("リフトの移動速度")] float m_animSpeed = default;
+    AudioSource m_audioSource;
+    Animator m_anim = default;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_anim = GetComponent<Animator>();
+        m_audioSource = GetComponent<AudioSource>();
+        _liftmode = (int)liftmoving;
+        m_anim.SetInteger("Liftmoving", _liftmode);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_anim.SetBool("PlayOnCollision", playOnCollision);
+        m_anim.speed = m_animSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (playOnCollision == true)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                playOnCollision = false;
+            }
+        }
+    }
+
+    public void SwitchOn()
+    {
+        playOnCollision = false;
+    }
+
+    public void LiftSound()
+    {
+        m_audioSource.Play();
+    }
+
+    enum Liftmode
+    {
+        /// <summary>
+        /// 上/右下に動く。
+        /// </summary>
+        Up_RightDown,
+        /// <summary>
+        /// 下/左下に動く。
+        /// </summary>
+        Down_LeftDown,
+        /// <summary>
+        /// 左/左上に動く。
+        /// </summary>
+        Left_LeftUp,
+        /// <summary>
+        /// 右/右上に動く。
+        /// </summary>
+        Right_RightUp,
+    }
+}
---
+++ Assets/Lift/Lift Script/LiftManagerVanish.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftManagerVanish : MonoBehaviour
+{
+    /// <summary>初期動作を設定できる。</summary>
+    [SerializeField, Tooltip("リフトの移動方向")] liftmode liftmoving = default;
+    int _liftmode;
+    [SerializeField, Tooltip("プレイヤーが乗った時に作動するか(基本ON)")] bool playOnCollision = default;
+    [SerializeField, Tooltip("リフトの移動速度")] float m_animSpeed = default;
+    Animator m_anim = default;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_anim = GetComponent<Animator>();
+        _liftmode = (int)liftmoving;
+        Debug.Log(_liftmode);
+        m_anim.SetInteger("Liftmoving", _liftmode);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_anim.SetBool("PlayOnCollision", playOnCollision);
+        m_anim.speed = m_animSpeed;
+/*        if (!playOnCollision)
+        {
+            playOnCollision = true;
+        }*/
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (playOnCollision == true)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                playOnCollision = false;
+            }
+        }
+    }
+
+    private void Reseter()
+    {
+        playOnCollision = true;
+        m_anim.SetBool("Reset?", true);
+    }
+
+    private void Starter()
+    {
+        m_anim.SetBool("Reset?", false);
+    }
+
+    enum liftmode
+    {
+        /// <summary>
+        /// 左に動いて消える。
+        /// </summary>
+        Left,
+        /// <summary>
+        /// 左上に動く。
+        /// </summary>
+        LeftUp,
+        /// <summary>
+        /// 上に動く。
+        /// </summary>
+        Up,
+        /// <summary>
+        /// 右上に動く。
+        /// </summary>
+        RightUp,
+        /// <summary>
+        /// 右に動く。
+        /// </summary>
+        Right,
+        /// <summary>
+        /// 落ちる挙動。
+        /// </summary>
+        Fall,
+    }
+}
